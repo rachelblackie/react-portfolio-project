2b844de80a6bcb97f7d98db466003de3191a7fd2
rachelblackie <rachelannblackie@gmail.com>
2023-03-21 11:35:03 +0100

    adding and styling homepage content

--- src/Hero.js
+++ src/Hero.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./App.css";
+import "./Hero.css";
+import background from "./background.jpg";
 
 export default function Hero() {
   return (
-    <div>
-      <h1 className="text-center my-auto">Rachel Blackie</h1>
-      <h3 className="text-center my-auto">Frontend Developer</h3>
-      <h4 className="text-center my-auto">Recent Projects</h4>
-      <div className="text-center">
-        <a href="#recent-projects">
-          <i className="fa-solid fa-circle-chevron-down"></i>
-        </a>
+    <div className="head-text text-center">
+      <div className="head-image position-relative">
+        <img
+          src={background}
+          className="img-fluid"
+          alt="ocean background"
+        ></img>
+      </div>
+      <div className="text-on-image text-center">
+        <h1>Rachel Blackie</h1>
+        <h3>Frontend Developer</h3>
+        <div className="recent-projects mt-6">
+          <h4>Recent Projects</h4>
+          <div className="text-center projects-arrow">
+            <a href="#recent-projects">
+              <i className="fa-solid fa-circle-chevron-down"></i>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/RecentProjects.js
+++ src/RecentProjects.js
@@ -1,17 +1,40 @@
 import React from "react";
+import WeatherAppScreenshot from "./WeatherAppScreenshot.png";
+import dolly from "./dolly.PNG";
+import Trixie from "./Trixie.PNG";
 
 export default function RecentProjects() {
   return (
-    <div>
-      <h1 className="text-center my-auto" id="recent-projects">
-        Recent Projects
-      </h1>
+    <div className="recent-projects">
       <div className="row">
-        <div className="col">
+        <div className="col-6">
           <img
-            src="/images/weather-app.png"
+            src={WeatherAppScreenshot}
             alt="weather app with 5-day forecast"
+            className="img-fluid"
           ></img>
+        </div>
+        <div className="col-6">
+          <h2>React Weather App</h2>
+          <p>ajehruaiwblveawucuwa</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src={Trixie}
+            alt="weather app with 5-day forecast"
+            className="img-fluid"
+          ></img>
+          <img
+            src={dolly}
+            alt="weather app with 5-day forecast"
+            className="img-fluid"
+          ></img>
+        </div>
+        <div className="col-6">
+          <h2>SheCodes Challenges</h2>
+          <p>srhsbevrauw</p>
         </div>
       </div>
     </div>
